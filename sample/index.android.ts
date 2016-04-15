f091f7f4cb721c177a83e6be86f0dcf2d1d08e03
Marc Laval <mlaval@amadeus.com>
2016-04-15 14:28:29 +0200

    chore: move project to angular/react-native-renderer

--- sample/index.android.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrapReactNative, HTTP_PROVIDERS, ROUTER_PROVIDERS} from 'angular2-react-native';
-import {KitchenSinkApp} from "./samples/android/kitchensink";
-
-bootstrapReactNative('ngReactNative', KitchenSinkApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS]);
--- sample/index.ios.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrapReactNative, ROUTER_PROVIDERS, HTTP_PROVIDERS} from 'angular2-react-native';
-import {KitchenSinkApp} from "./samples/ios/kitchensink";
-
-bootstrapReactNative('ngReactNative', KitchenSinkApp, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
